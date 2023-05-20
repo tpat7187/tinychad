ab8610bde21ec8e52d8728a463cee0523075c010
tpat7187 <corranpaterson@live.com>
2023-05-20 03:24:34 +0100

    shape tracker in backward, more ops

--- tensor.py
+++ tensor.py
@@ -4,15 +4,12 @@ import numpy as np
 
 class OP: 
   def __init__(self, saved = None):
-    self.arg = arg
+    self.arg = type(self).__name__
     self.saved = np.array(saved)
-
-  def __repr__(self): 
-    return self.arg
 
 class ADD(OP): 
   def __init__(self, saved = None): 
-    self.arg = '<ADD>' 
+    self.arg = type(self).__name__
     self.saved = np.array(saved)
 
   def forward(x, y): 
@@ -24,7 +21,7 @@ class ADD(OP):
 
 class MATMUL(OP): 
   def __init__(self, saved = None): 
-    self.arg = '<MATMUL>' 
+    self.arg = type(self).__name__
     self.saved = np.array(saved)
 
   def forward(x, y): 
@@ -36,62 +33,68 @@ class MATMUL(OP):
 
 class LOAD(OP): 
   def __init__(self, saved = None):
-    self.arg = '<LOAD>'
+    self.arg = type(self).__name__
     self.saved = saved
 
 #### TENSOR CLASS ####
 
 class tensor: 
-  def __init__(self, data, op = LOAD()):
+  def __init__(self, data, op = LOAD(), requires_grad = False):
     self.data = np.array(data, dtype = np.float32)
     self.op = op 
     self.grad = np.zeros(self.data.shape)
 
   def ones(*shape): return tensor(np.ones(*shape))
+  def randn(*shape): return tensor(np.random.randn(*shape))
+  def eye(shape): return tensor(np.eye(shape))
+  def zeros(*shape): return tensor(np.zeros(*shape))
 
   def __repr__(self): 
-    return f"OP = {self.op}: shape = {self.data.shape}" 
+    return f"op = <{self.op.arg}>: shape = {self.data.shape}: grad_shape = {self.grad.shape}"
 
   def __add__(self,x): return self.add(x)
   def __matmul__(self,x): return self.dot(x)
-
 
   # fundamental OPS
   def add(self, x): return ADD.forward(self, x)
   def dot(self, x): return MATMUL.forward(self, x)
 
-
   # unary ops
   def T(self): return tensor(self.data.transpose())
 
   # i think this works
-  def toposort(self): 
-    topo, vis = [], set()
+  def toposort(self, track): 
+    topo, vis = [], []
     def _toposort(s): 
       if s not in vis: 
-        vis.add(s)
+        vis.append(s)
+        #print(s) if track == True else None
         if not isinstance(s.op, LOAD):
           for child in s.op.saved: 
             _toposort(child)
           topo.append(s)
     _toposort(self)
+    
+    # should we include load ops
+    print(*reversed(topo), sep="\n") if track == True else None
     return topo
 
-  def backward(self): 
+  def backward(self, track = False): 
     self.grad = np.ones(self.grad.shape)
-    for x in reversed(self.toposort()): 
+    for x in reversed(self.toposort(track)): 
       x.op.backward(x.grad, x.data)
 
-
 def test(): 
-  x = tensor.ones((5,5))
-  y = tensor.ones((5,5))
-  w = x @ y
+  x = tensor.randn(1,784)
+  y = tensor.randn(784,128)
+  z = tensor.randn(128,10)
 
-  w.backward()
+  w = x @ y
+  j = w @ z
+
+  j.backward(track = True)
 
 if __name__ == "__main__":
   test()
-
 
 
