b1d5293aced87bf9cbfa0d73887dd3c2553ae9e9
tpat7187 <corranpaterson@live.com>
2023-05-20 04:00:33 +0100

    SUM for gradcheck, backward pass needs the axis

--- tensor.py
+++ tensor.py
@@ -31,6 +31,18 @@ class MATMUL(OP):
     self.saved[0].grad += np.dot(out_grad, self.saved[1].T().data)
     self.saved[1].grad += np.dot(self.saved[0].T().data, out_grad)
 
+class SUM(OP):
+  def __init__(self, saved = None): 
+    self.arg = type(self).__name__
+    self.saved = np.array(saved)
+
+  def forward(x):
+    return tensor(np.array([x.data.sum()]), op = SUM(saved = (x,)))
+
+  # the backward pass needs to know what axis we summed over
+  def backward(self, out_grad, out):
+    self.saved[0].grad += out_grad 
+
 class LOAD(OP): 
   def __init__(self, saved = None):
     self.arg = type(self).__name__
@@ -58,6 +70,7 @@ class tensor:
   # fundamental OPS
   def add(self, x): return ADD.forward(self, x)
   def dot(self, x): return MATMUL.forward(self, x)
+  def sum(self): return SUM.forward(self)
 
   # unary ops
   def T(self): return tensor(self.data.transpose())
@@ -69,14 +82,14 @@ class tensor:
       if s not in vis: 
         vis.append(s)
         #print(s) if track == True else None
-        if not isinstance(s.op, LOAD):
+        if not isinstance(s.op, LOAD) or None:
           for child in s.op.saved: 
             _toposort(child)
           topo.append(s)
     _toposort(self)
     
     # should we include load ops
-    print(*reversed(topo), sep="\n") if track == True else None
+    print(*topo, sep="\n") if track == True else None
     return topo
 
   def backward(self, track = False): 
@@ -85,14 +98,21 @@ class tensor:
       x.op.backward(x.grad, x.data)
 
 def test(): 
-  x = tensor.randn(1,784)
-  y = tensor.randn(784,128)
-  z = tensor.randn(128,10)
+  # mnist shapes
+  inp = tensor.randn(1,784)
 
-  w = x @ y
-  j = w @ z
+  l1_w = tensor.randn(784,128) 
+  l1_b = tensor.randn(1,128)
 
-  j.backward(track = True)
+  l2_w = tensor.randn(128,10)
+  l2_b = tensor.randn(1,10)
+
+  layer1 = inp @ l1_w + l1_b 
+  layer2 = layer1 @ l2_w + l2_b
+
+  layer3 = layer2.sum()
+
+  layer3.backward(track = True)
 
 if __name__ == "__main__":
   test()
