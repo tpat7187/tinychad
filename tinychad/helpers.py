91eb9d3af8b65c6c627e368b0e8f4b25ba2b2db2
tpat7187 <corranpaterson@live.com>
2023-08-20 19:16:29 +0100

    this is way better

--- tinychad/helpers.py
+++ tinychad/helpers.py
@@ -4,29 +4,19 @@ from tinychad.tensor import tensor, Conv2d, Linear, BatchNorm2d
 from typing import Union, Optional, Tuple
 
 # blocks will be object -> several Linear/Conv2d/BatchNorm -> tensor, tensor
-def get_parameters(obj:object) -> dict: 
+# increase depth of tensor search 
+def get_parameters(obj:object, max_depth:int=4, depth:int=0) -> list: 
+    if depth > max_depth:
+        return []
     layers, params = (Conv2d, Linear, BatchNorm2d), []
-    states = inspect.getmembers(obj, lambda a: not(inspect.isroutine(a))), []
-    states = [a for a in states[0] if not(a[0].startswith('__') and a[0].endswith('__'))]
-
-    def check_for_tensor(obj):
-        states = inspect.getmembers(obj, lambda a: not(inspect.isroutine(a)))
-        states = [a for a in states if not(a[0].startswith('__') and a[0].endswith('__'))]
-        [params.append(j[1]) for _,j in enumerate(states) if type(j[1]) == tensor]
-
-    def check_for_layer(obj):
-        states = inspect.getmembers(obj, lambda a: not(inspect.isroutine(a))), []
-        states = [a for a in states[0] if not(a[0].startswith('__') and a[0].endswith('__'))]
-        out = [j for _,j in enumerate(states) if type(j[1]) in layers]
-        [check_for_tensor(j[1]) if len(out) > 0 else [] for j in states]
+    states = inspect.getmembers(obj, lambda a: not(inspect.isroutine(a)))
+    states = [a for a in states if not(a[0].startswith('__') and a[0].endswith('__'))]
 
     for s in states: 
-        if isinstance(s[1], tensor): 
+        if isinstance(s[1], tensor):
             params.append(s[1])
-        elif isinstance(s[1], (layers)): 
-            check_for_tensor(s[1])
-        else: 
-            check_for_layer(s[1])
-
+        elif isinstance(s[1], layers):
+            params.extend(get_parameters(s[1], max_depth, depth + 1))
+        elif hasattr(s[1], '__dict__'): 
+            params.extend(get_parameters(s[1], max_depth, depth + 1))
     return params
-
